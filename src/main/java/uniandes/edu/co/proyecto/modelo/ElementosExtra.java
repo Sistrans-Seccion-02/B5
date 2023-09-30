b34ccd5c4a183a4b15aea15bf1b8c41201b47dc8
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2023-09-30 18:37:23 -0500

    crear ElementosExtra

---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/ElementosExtra.java
@@ -0,0 +1,42 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "elementosExtra")
+public class ElementosExtra {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+
+    private Integer idElemento;
+    private String nombreElemento;
+
+    public ElementosExtra(String nombreElemento)
+    {
+        this.nombreElemento = nombreElemento;
+    }
+
+    public ElementosExtra()
+    {;}
+
+    public Integer getIdElemento() {
+        return idElemento;
+    }
+
+    public String getNombreElemento() {
+        return nombreElemento;
+    }
+
+    public void setIdElemento(Integer idElemento) {
+        this.idElemento = idElemento;
+    }
+
+    public void setNombreElemento(String nombreElemento) {
+        this.nombreElemento = nombreElemento;
+    }
+}
