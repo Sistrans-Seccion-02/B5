db14e7abc6651381cea955755ce96139444a3eee
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2023-10-01 17:25:12 -0500

    arreglra ReservaHoteles y actualizar modelo

--- src/main/java/uniandes/edu/co/proyecto/modelo/ReservaHoteles.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/ReservaHoteles.java
@@ -2,10 +2,15 @@ package uniandes.edu.co.proyecto.modelo;
 
 import java.util.Date;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,27 +20,37 @@ public class ReservaHoteles {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer numReserva;
-
     private Date fechaEntrada;
     private Date fechaSalida;
     private Boolean pago;
     private Boolean reservaTomada;
-
-    public ReservaHoteles(Date fechaEntrada, Date fechaSalida, Boolean pago, Boolean reservaTomada)
-    {
-        this.fechaEntrada = fechaEntrada;
-        this.fechaSalida = fechaSalida;
-        this.pago = pago;
-        this.reservaTomada = reservaTomada;
-    }
+    @OneToOne(optional = true)
+    @JoinColumn(name = "promociones_nombrePlan", referencedColumnName = "nombrePlan")
+    private Promocion promociones_nombrePlan;
+    @ManyToOne
+    @JoinColumn(name = "hoteles_nombre", referencedColumnName = "nombre")
+    private Hotel hoteles_nombre;
+    @ManyToOne
+    @JoinColumn(name = "habitacioes_numero_habitacion", referencedColumnName = "numeroHabitacion")
+    private Habitacion habitacioes_numero_habitacion;
 
     public ReservaHoteles()
     {;}
 
+    public ReservaHoteles(Date fechaEntrada, Date fechaSalida, Boolean pago, Boolean reservaTomada,
+            Promocion promociones_nombrePlan, Hotel hoteles_nombre, Habitacion habitacioes_numero_habitacion) {
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.pago = pago;
+        this.reservaTomada = reservaTomada;
+        this.promociones_nombrePlan = promociones_nombrePlan;
+        this.hoteles_nombre = hoteles_nombre;
+        this.habitacioes_numero_habitacion = habitacioes_numero_habitacion;
+    }
+
     public Integer getNumReserva() {
         return numReserva;
     }
-
 
     public void setNumReserva(Integer numReserva) {
         this.numReserva = numReserva;
@@ -73,5 +88,27 @@ public class ReservaHoteles {
         this.reservaTomada = reservaTomada;
     }
 
-    
+    public Promocion getPromociones_nombrePlan() {
+        return promociones_nombrePlan;
+    }
+
+    public void setPromociones_nombrePlan(Promocion promociones_nombrePlan) {
+        this.promociones_nombrePlan = promociones_nombrePlan;
+    }
+
+    public Hotel getHoteles_nombre() {
+        return hoteles_nombre;
+    }
+
+    public void setHoteles_nombre(Hotel hoteles_nombre) {
+        this.hoteles_nombre = hoteles_nombre;
+    }
+
+    public Habitacion getHabitacioes_numero_habitacion() {
+        return habitacioes_numero_habitacion;
+    }
+
+    public void setHabitacioes_numero_habitacion(Habitacion habitacioes_numero_habitacion) {
+        this.habitacioes_numero_habitacion = habitacioes_numero_habitacion;
+    }
 }
