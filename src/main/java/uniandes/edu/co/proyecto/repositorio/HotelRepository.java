b89a621fe8c7b4e051031e84511c237b47fadc1f
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-01 17:30:46 -0500

    hotelrepo

--- src/main/java/uniandes/edu/co/proyecto/repositorio/HotelRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/HotelRepository.java
@@ -3,7 +3,10 @@ package uniandes.edu.co.proyecto.repositorio;
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import uniandes.edu.co.proyecto.modelo.Hotel;
 
@@ -11,5 +14,20 @@ public interface HotelRepository extends JpaRepository<Hotel, Integer>{
 
     @Query(value = "SELECT * FROM hoteles ", nativeQuery = true)
     Collection<Hotel> darHoteles();
+
+     @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO hoteles (nombre) VALUES(:nombre) ", nativeQuery = true)
+    void insertarHotel(@Param("nombre") String nombre);
+    
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE hoteles SET nombre= :nombre WHERE nombre =:nombre", nativeQuery = true)
+    void actualizarHotel(@Param("nombre") String nombre);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM hoteles WHERE nombre =:nombre", nativeQuery = true)
+    void eliminarHotel(@Param("nombre") String nombre);
     
 }
