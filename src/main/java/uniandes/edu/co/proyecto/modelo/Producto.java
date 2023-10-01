e3ea0767e76939a0385cb53bbb11f98a53731680
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-01 17:07:41 -0500

    producto

--- src/main/java/uniandes/edu/co/proyecto/modelo/Producto.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Producto.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,12 +19,28 @@ public class Producto {
     private String nombre;
     private Integer costo;
     private Boolean aplicaIncluido;
+    @ManyToOne
+    @JoinColumn(name="BarId", referencedColumnName="idTipoServicio")
+    private Bar barId;
+    @ManyToOne
+    @JoinColumn(name="RestauranteId", referencedColumnName="idTipoServicio")
+    private Restaurante restauranteId;
+    @ManyToOne
+    @JoinColumn(name="TiendaId", referencedColumnName="idTipoServicio")
+    private Tienda tiendaId;
+    @ManyToOne
+    @JoinColumn(name="LavanderiaId", referencedColumnName="idTipoServicio")
+    private Lavanderia lavanderiaId;
 
-    public Producto(Boolean aplicaIncluido, String nombre, Integer costo)
+    public Producto(Boolean aplicaIncluido, String nombre, Integer costo, Bar barId, Restaurante restauranteId, Tienda tiendaId, Lavanderia lavanderiaId)
     {
         this.aplicaIncluido = aplicaIncluido;
         this.nombre = nombre;
         this.costo = costo;
+        this.barId = barId;
+        this.restauranteId = restauranteId;
+        this.tiendaId = tiendaId;
+        this.lavanderiaId = lavanderiaId;
     }
 
     public Producto()
@@ -58,6 +76,38 @@ public class Producto {
 
     public void setAplicaIncluido(Boolean aplicaIncluido) {
         this.aplicaIncluido = aplicaIncluido;
+    }
+
+    public Bar getBarId() {
+        return barId;
+    }
+
+    public void setBarId(Bar barId) {
+        this.barId = barId;
+    }
+
+    public Restaurante getRestauranteId() {
+        return restauranteId;
+    }
+
+    public void setRestauranteId(Restaurante restauranteId) {
+        this.restauranteId = restauranteId;
+    }
+
+    public Tienda getTiendaId() {
+        return tiendaId;
+    }
+
+    public void setTiendaId(Tienda tiendaId) {
+        this.tiendaId = tiendaId;
+    }
+
+    public Lavanderia getLavanderiaId() {
+        return lavanderiaId;
+    }
+
+    public void setLavanderiaId(Lavanderia lavanderiaId) {
+        this.lavanderiaId = lavanderiaId;
     }
 
     
