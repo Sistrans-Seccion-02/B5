e599db3339a6401806541957a49c1ebc2a775653
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-01 13:47:02 -0500

    Reservas

---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/ReservaHoteles.java
@@ -0,0 +1,77 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reservaHoteles")
+public class ReservaHoteles {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer numReserva;
+
+    private Date fechaEntrada;
+    private Date fechaSalida;
+    private Boolean pago;
+    private Boolean reservaTomada;
+
+    public ReservaHoteles(Date fechaEntrada, Date fechaSalida, Boolean pago, Boolean reservaTomada)
+    {
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.pago = pago;
+        this.reservaTomada = reservaTomada;
+    }
+
+    public ReservaHoteles()
+    {;}
+
+    public Integer getNumReserva() {
+        return numReserva;
+    }
+
+
+    public void setNumReserva(Integer numReserva) {
+        this.numReserva = numReserva;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Boolean getPago() {
+        return pago;
+    }
+
+    public void setPago(Boolean pago) {
+        this.pago = pago;
+    }
+
+    public Boolean getReservaTomada() {
+        return reservaTomada;
+    }
+
+    public void setReservaTomada(Boolean reservaTomada) {
+        this.reservaTomada = reservaTomada;
+    }
+
+    
+}
---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/ReservaServicios.java
@@ -0,0 +1,59 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reservaServicios")
+public class ReservaServicios {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer numReserva;
+
+    private Date fecha;
+    private Boolean atendida;
+
+    public ReservaServicios(Date fecha, Boolean atendida)
+    {
+        this.fecha = fecha;
+        this.atendida = atendida;
+    }
+
+    public ReservaServicios()
+    {;}
+
+    public Integer getNumReserva() {
+        return numReserva;
+    }
+
+    public void setNumReserva(Integer numReserva) {
+        this.numReserva = numReserva;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Boolean getAtendida() {
+        return atendida;
+    }
+
+    public void setAtendida(Boolean atendida) {
+        this.atendida = atendida;
+    }
+
+    
+
+
+    
+}
