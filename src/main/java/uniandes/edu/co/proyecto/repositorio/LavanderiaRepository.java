f11e736122efe4d5a825089257a87a692177d29d
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-10-01 21:32:35 -0500

    Piscina

--- src/main/java/uniandes/edu/co/proyecto/repositorio/LavanderiaRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/LavanderiaRepository.java
@@ -13,24 +13,24 @@ import uniandes.edu.co.proyecto.modelo.Lavanderia;
 public interface LavanderiaRepository extends JpaRepository<Lavanderia, Integer>{
 
     @Query(value = "SELECT * FROM lavanderias ", nativeQuery = true)
-    Collection<Lavanderia> darInternets();
+    Collection<Lavanderia> darLavanderias();
     
     @Query(value = "SELECT * FROM lavanderias WHERE idTipoServicio = :idTipoServicio", nativeQuery = true)
-    Lavanderia darInternet(@Param("idTipoServicio") int idTipoServicio);
+    Lavanderia darLavanderia(@Param("idTipoServicio") int idTipoServicio);
 
     @Modifying
     @Transactional
     @Query(value = "INSERT INTO lavanderias (idTipoServicio, capacidad, aplicaCompartido, nombreHotel) VALUES(proyecto_sequence.nextval, :capacidad, :aplicaCompartido, :nombreHotel) ", nativeQuery = true)
-    void insertarInternet(@Param("capacidad") int capacidad, @Param("aplicaCompartido") Boolean aplicaCompartido, @Param("nombreHotel") String nombreHotel);
+    void insertarLavanderia(@Param("capacidad") int capacidad, @Param("aplicaCompartido") Boolean aplicaCompartido, @Param("nombreHotel") String nombreHotel);
     
     @Modifying
     @Transactional
     @Query(value = "UPDATE lavanderias SET capacidad= :capacidad, aplicaCompartido= :aplicaCompartido, nombreHotel= :nombreHotel  WHERE idTipoServicio =:idTipoServicio", nativeQuery = true)
-    void actualizarInternet(@Param("idTipoServicio") int idTipoServicio, @Param("capacidad") int capacidad, @Param("aplicaCompartido") Boolean aplicaCompartido, @Param("nombreHotel") String nombreHotel);
+    void actualizarLavanderia(@Param("idTipoServicio") int idTipoServicio, @Param("capacidad") int capacidad, @Param("aplicaCompartido") Boolean aplicaCompartido, @Param("nombreHotel") String nombreHotel);
 
     @Modifying
     @Transactional
     @Query(value = "DELETE FROM lavanderias WHERE idTipoServicio =:idTipoServicio", nativeQuery = true)
-    void eliminarInternet(@Param("idTipoServicio") int idTipoServicio);
+    void eliminarLavanderia(@Param("idTipoServicio") int idTipoServicio);
     
 }
